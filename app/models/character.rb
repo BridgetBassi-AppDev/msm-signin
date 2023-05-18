485081eb53845091135607fb2a9058ee3294517d
Bridget Bassi <bbassi@chicagobooth.edu>
2023-05-18 04:40:21 +0000

    test

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,8 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+  belongs_to :movie  
+
+  has_many :cast, :through => :characters, :source => :actor
+  validates(:title, { :presence => true})
 end
